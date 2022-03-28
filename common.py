40f8278f8b237c5122d5687d4fb5e7a9f88cece1
Cyano Hao <c@cyano.cn>
2022-03-28 13:05:16 +0800

    special handling tooltip.js

--- common.py
+++ common.py
@@ -21,6 +21,9 @@ def PostAction(content: bytes) -> bytes:
 	if zamimg:
 		html = re.sub(r'https://wow.zamimg.com/', zamimg, html)
 
+		# special handling tooltip.js
+		html = re.sub(r'"https://wow.zamimg.com"', f'"{zamimg}"', html)
+
 	# ajax.googleapis.com cdn
 	html = re.sub(r'https://ajax.googleapis.com/', 'https://ajax.proxy.ustclug.org/', html)
 
